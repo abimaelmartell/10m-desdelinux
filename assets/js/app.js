864184e430b74d3e432f89b75fc4145c1109a78e
Abimael Martell <abimex@gmail.com>
2013-09-06 15:24:20 -0700

    make jslint happier

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,26 @@
-(function($){
+(function ($) {
   var json_url = "http://vimeo.com/api/v2/16180379/videos.json?callback=?";
   var listTemplate = _.template($("#list_template").html());
   var $list = $('ul.videos_list');
 
-  $.getJSON(json_url, function(data){
-    _.each(data, function(e){
-      renderVideo(e);
-    });
-  });
-
-  var renderVideo = function(video_obj){
-    el = listTemplate({
+  var renderVideo = function (video_obj) {
+    var el = listTemplate({
       title: video_obj.title,
       image: video_obj.thumbnail_large,
       video_url: "//player.vimeo.com/video/" + video_obj.id
     });
-    console.log(video_obj);
     $list.append(el);
   };
 
-  $(function(){
+  $.getJSON(json_url, function (data) {
+    _.each(data, function (e) {
+      renderVideo(e);
+    });
+  });
+
+  $(function () {
     $('ul.videos_list li a').fancybox({
-        type: "iframe"
+      type: "iframe"
     });
   });
 })(jQuery);
