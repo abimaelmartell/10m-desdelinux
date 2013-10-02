9e91a010360932776cf750e0cb69b4c6dbc50e04
Abimael Martell <abimex@gmail.com>
2013-10-02 12:14:03 -0700

    jslint now is happy :smiley:

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,37 @@
-(function ($) {
-  var DesdeLinux = function(){
+/*global window, jQuery*/
+(function ($, window) {
+  var DesdeLinux = function () {
     var homeTemplate = _.template($("#home_template").html()),
-        videoTemplate = _.template($("#video_template").html()),
-        $container = $('.main_content'),
-        _this = this;
+      videoTemplate = _.template($("#video_template").html()),
+      $container = $('.main_content'),
+      _this = this;
 
     this.current_route = null;
 
-    this.init = function(){
+    this.init = function () {
       this.initRoute(window.location.hash);
-      $(window).on('popstate', function(e){
+      $(window).on('popstate', function (e) {
         e.preventDefault();
         _this.initRoute(window.location.hash);
       });
     };
 
-    this.initRoute = function(route){
-      if(route == this.current_route)
+    this.initRoute = function (route) {
+      if (route === this.current_route) {
         return false;
+      }
       var video_id = route.match(/^#!\/video\/(\d+)/);
       $('.loading').show();
-      if(video_id){
+      if (video_id) {
         this.video(video_id[1]);
-      }else{
+      } else {
         this.home();
       }
       $(window).scrollTop(0);
       this.current_route = route;
     };
 
-    this.home = function(){
+    this.home = function () {
       $.getJSON("http://vimeo.com/api/v2/group/shortfilms/videos.json?callback=?", function (data) {
         var videos = [], video;
         _.each(data, function (video_obj) {
@@ -45,19 +47,18 @@
       });
     };
 
-    this.video = function(video_id){
-      $.getJSON("http://vimeo.com/api/v2/video/" + video_id +  ".json?callback=?", function(data){
-          var video = data[0];
-          $container.html(videoTemplate({
-            title: video.title,
-            video_id: video.id
-          }));
-          $('.loading').hide();
+    this.video = function (video_id) {
+      $.getJSON("http://vimeo.com/api/v2/video/" + video_id +  ".json?callback=?", function (data) {
+        var video = data[0];
+        $container.html(videoTemplate({
+          title: video.title,
+          video_id: video.id
+        }));
+        $('.loading').hide();
       });
     };
-
-    return this.init();
   };
 
-  new DesdeLinux();
-})(jQuery);
+  var app = new DesdeLinux();
+  app.init();
+}(jQuery, window));
